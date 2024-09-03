4a3f497dced3cc7c3eb365c8fe2b9a6faa92f33a
nahuelmendez6 <mendezmaturano6@gmail.com>
2024-09-03 16:57:32 -0300

    Templates de categorias

--- blog_enterprise/views.py
+++ blog_enterprise/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from blogs.models import Category, Blog
 
 def home(request):
-    categories = Category.objects.all()
+
     posts = Blog.objects.filter(is_featured=True).order_by('-updated_at')
     context = {
-        'categories': categories,
         'featured_post':posts
     }
 
--- blogs/views.py
+++ blogs/views.py
@@ -1,16 +1,23 @@
 from unicodedata import category
 
 from django.http import HttpResponse
-from django.shortcuts import render
-from blogs.models import Blog
+from django.shortcuts import render, get_object_or_404, redirect
+from blogs.models import Blog, Category
 
 
 # Create your views here.
 
 def posts_by_category(request, category_id):
     posts = Blog.objects.filter(status='Published', category=category_id)
+
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+
     context = {
-        'posts':posts
+        'posts':posts,
+        'category':category,
     }
 
     return render(request, 'posts_by_category.html', context)
